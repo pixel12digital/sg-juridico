69b5ecfc279a8972685656f94a6f8fce9136b9f9
Seu Nome <seuemail@gmail.com>
2025-11-03 11:41:41 -0300

    Aplicar estilo do projeto Ã  pÃ¡gina de produtos individual - Criar templates customizados single-product.php e content-single-product.php - Adicionar estilos CSS completos para pÃ¡gina de produto seguindo paleta de cores do projeto

---
+++ wp-content/themes/sg-juridico/woocommerce/single-product.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * @package SG_Juridico
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<main id="main" class="site-main">
+	<div class="container">
+		<div class="site-main-wrapper">
+			<div class="posts-container">
+				<?php
+				/**
+				 * woocommerce_before_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 */
+				do_action( 'woocommerce_before_main_content' );
+				?>
+
+				<?php while ( have_posts() ) : ?>
+					<?php the_post(); ?>
+					<?php wc_get_template_part( 'content', 'single-product' ); ?>
+				<?php endwhile; // end of the loop. ?>
+
+				<?php
+				/**
+				 * woocommerce_after_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+				 */
+				do_action( 'woocommerce_after_main_content' );
+				?>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php
+get_footer();
+
